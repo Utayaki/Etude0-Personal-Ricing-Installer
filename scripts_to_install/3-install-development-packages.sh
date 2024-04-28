e5a72bc150364f02e8b08d7f402709cba4bd0f87
Firihino <helloyoutube31@yandex.ru>
2024-04-28 15:58:21 +0000

    [new] Added packages for development tools

--- scripts_to_install/3-install-development-packages.sh
+++ scripts_to_install/3-install-development-packages.sh
@@ -1,2 +1,12 @@
 # Install Docker
 yay -S nvidia-container-toolkit libnvidia-container docker docker-compose
+
+# Install Personal IDE with nvim
+yay -S neovim
+yay -S npm rust
+
+
+# Install old Python versions
+yay -S python38 python310
+
+
