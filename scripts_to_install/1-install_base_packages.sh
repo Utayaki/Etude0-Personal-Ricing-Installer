823a6754ae038deefda8870be68f267a5918484f
Firihino <helloyoutube31@yandex.ru>
2024-04-28 16:21:41 +0000

    [fix] Added sudo loop flag for sudo being less annoying during installation

--- scripts_to_install/1-install_base_packages.sh
+++ scripts_to_install/1-install_base_packages.sh
@@ -1,14 +1,14 @@
 # Load repos and upgrade system
-yay -Syyu
+yay -Syyu --sudoloop
 
 # Load X11
-yay -S xwinwrap-0.9-bin xf86-video xorg-xinput xorg xorg-xinit
+yay -S xwinwrap-0.9-bin xf86-video xorg-xinput xorg xorg-xinit --sudoloop
 
 # Load NVIDIA Drivers
-yay -S nvidia-lts nvidia-utils cuda cudnn cuda-tools
+yay -S nvidia-lts nvidia-utils cuda cudnn cuda-tools --sudoloop
 
 # Load base packages
-yay -S bspwm sxhkd rofi polybar mpv thunar btop mousepad fcitx5 libreoffice-still nvim alacritty flameshot deluge filezilla xarchiver
+yay -S bspwm sxhkd rofi polybar mpv thunar btop mousepad fcitx5 libreoffice-still nvim alacritty flameshot deluge filezilla xarchiver --sudoloop
 
 # Load other AUR packages
-yay -S obsidian lazydocker 
+yay -S obsidian lazydocker --sudoloop
--- scripts_to_install/3-install-development-packages.sh
+++ scripts_to_install/3-install-development-packages.sh
@@ -1,12 +1,12 @@
 # Install Docker
-yay -S nvidia-container-toolkit libnvidia-container docker docker-compose
+yay -S nvidia-container-toolkit libnvidia-container docker docker-compose --sudoloop
 
 # Install Personal IDE with nvim
-yay -S neovim
-yay -S npm rust
+yay -S neovim --sudoloop
+yay -S npm rust --sudoloop
 
 
 # Install old Python versions
-yay -S python38 python310
+yay -S python38 python310 --sudoloop
 
 
